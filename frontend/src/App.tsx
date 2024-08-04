edc9c81f4023cb10edd74b369f686749632c2383
Dawlau <blahoviciandrei1@gmail.com>
2024-08-05 00:14:42 +0200

    Switched frontend to typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import ChatWrapper from './components/ChatWrapper';
+
+const App: React.FC = () => {
+    return (
+        <div className="App">
+            <body>
+                <ChatWrapper />
+            </body>
+        </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/ChatWrapper.tsx
@@ -0,0 +1,66 @@
+import React, { useState, FormEvent, ChangeEvent, useEffect } from 'react';
+import ChatBox from "./ChatBox";
+import axios from "axios";
+
+interface Message {
+    type: string;
+    text: string;
+}
+
+const ChatWrapper: React.FC = () => {
+    const [question, setQuestion] = useState<string>('');
+    const [sessionId, setSessionId] = useState<string>('');
+    const [messages, setMessages] = useState<Message[]>([]);
+
+    const loginUser = async () => {
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/login`, {}, { withCredentials: true });
+            setSessionId(res.data.session_id);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    useEffect(() => {
+        loginUser();
+    }, []);
+
+    const fetchWelcomeMessage = async (sessionId: string) => {
+        
+        try {
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/chat`, { question: 'Hello', session_id: sessionId }, { withCredentials: true });
+            const botMessage: Message = { type: 'bot', text: res.data.response };
+            console.log("Bot message: ", botMessage);
+            setMessages([botMessage]);
+        } catch (error) {
+            console.error('Error fetching welcome message:', error);
+        }
+    };
+
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (question.trim() === '') return;
+
+        const userMessage: Message = { type: 'user', text: question };
+        setMessages([...messages, userMessage]);
+        setQuestion('');
+
+        try {
+            console.log("Session ID: ", sessionId);
+            const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/chat` as string, { question: question, session_id: sessionId }, { withCredentials: true });
+            const botMessage: Message = { type: 'bot', text: res.data.response };
+            setMessages(prevMessages => [...prevMessages, botMessage]);
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    };
+
+    return (
+        sessionId ?
+        <ChatBox messages={messages} fetchWelcomeMessage={fetchWelcomeMessage} sessionId={sessionId} handleSubmit={handleSubmit} question={question} setQuestion={setQuestion} />
+        :
+        <></>
+    );
+};
+
+export default ChatWrapper;
